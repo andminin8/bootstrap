8fb8539ae23f86d6ab27506f29a6cbdf3a35e70f
�andminin8 <andminin8@gmail.com>
2018-06-28 11:12:37 -0300

    comienzo de repositorio clase uno responsiva

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>clase Uno Responsive</title>
+    <link rel="stylesheet" href="css/master.css">
+  </head>
+  <body>
+    <section class="top">
+      <nav>
+        <ul>
+          <li>
+            <a href="#"></a>
+          </li>
+          <li>
+            <a href="#"></a>
+          </li>
+          <li>
+            <a href="#"></a>
+          </li>
+        </ul>
+      </nav>
+
+    </section>
+
+
+
+  </body>
+</html>
