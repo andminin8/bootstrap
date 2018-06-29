879aa6081da0fede2e68484a348c5fc8c1baae66
�andminin8 <andminin8@gmail.com>
2018-06-29 16:39:05 -0300

    retoma de css

--- index.php
+++ index.php
@@ -2,26 +2,31 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
     <title>clase Uno Responsive</title>
     <link rel="stylesheet" href="css/master.css">
   </head>
   <body>
-    <section class="top">
+    <main>
+      <header>
+        <div class="top"></div>
+      </header>
+      <!--
       <nav>
         <ul>
-          <li>
-            <a href="#"></a>
-          </li>
-          <li>
-            <a href="#"></a>
-          </li>
-          <li>
-            <a href="#"></a>
-          </li>
+            <li><a href="">Home</a></li>
+            <li><a href="">Categories</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Portfolio</a></li>
+            <li><a href="">Contact</a></li>
         </ul>
       </nav>
+    -->
+    </main>
 
-    </section>
+
+
+
 
 
 
