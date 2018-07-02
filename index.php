17b638a3aa25c9f4025e474931cbfb4f5e8021a4
�andminin8 <andminin8@gmail.com>
2018-07-02 16:18:50 -0300

    cambio de hoja de estilo por grilla numerada

--- index.php
+++ index.php
@@ -4,11 +4,13 @@
     <meta charset="utf-8">
     <title>clase Uno Responsive</title>
     <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
-    <link rel="stylesheet" href="css/master.css">
+    <link rel="apple-touch-icon-precomposed" href="img/favicon.png">
+    <link rel="icon" href="img/favicon.png">
+    <link rel="stylesheet" href="css/master1.1.css">
   </head>
   <body>
-    <main>
-      <header class="top">
+
+      <header class="top" id="miHeader">
       </header>
       <nav class="menu" id="miMenu">
         <a href="#" class="activo">Inicio</a>
@@ -17,66 +19,104 @@
         <a href="#">Contacto</a>
         <a href="#" id="boton" class="icono">&#9776;</a>
       </nav>
-      <section class="articulos">
-        <header>
-          <h1>los vengadores ( equipo )</h1>
-          <p>
-            <strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</strong> sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-        </header>
-          <article class="personajes">
-            <div class="box">
-              <h3>personaje 01</h3>
-              <img class="imgArticle" src="img/captain_america.jpg" alt="capitan america en lucha">
-            </div>
-
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      <main class="container" id="miMain">
+        <section class="articulos uno">
+          <header>
+            <h1>los vengadores ( equipo )</h1>
+            <p>
+              <strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</strong> sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
-            <button type="button" name="button">leer mas</button>
-          </article>
+          </header>
 
-          <article class="personajes">
-            <div class="box">
-              <h3>personaje 01</h3>
-              <img class="imgArticle" src="img/captain_america.jpg" alt="capitan america en lucha">
-            </div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-            <button type="button" name="button">leer mas</button>
-          </article>
-          <article class="personajes">
-            <div class="box">
-              <h3>personaje 01</h3>
-              <img class="imgArticle" src="img/captain_america.jpg" alt="capitan america en lucha">
-            </div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-            <button type="button" name="button">leer mas</button>
-          </article>
-          <article class="personajes">
-            <div class="box">
-              <h3>personaje 01</h3>
-              <img class="imgArticle" src="img/captain_america.jpg" alt="capitan america en lucha">
-            </div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-            <button type="button" name="button">leer mas</button>
-          </article>
-          <article class="personajes">
-            <div class="box">
-              <h3>personaje 01</h3>
-              <img class="imgArticle" src="img/captain_america.jpg" alt="capitan america en lucha">
-            </div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-            <button type="button" name="button">leer mas</button>
-          </article>
+            <article class="personajes primero">
+              <div class="box">
+                <h3>personaje 01</h3>
+                <img class="imgArticle" src="img/captain_america.jpg" alt="capitan america en lucha">
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                <br><br>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+              <button type="button" name="button">leer mas</button>
+            </article>
 
+            <article class="personajes segundo">
+              <div class="box">
+                <h3>personaje 02</h3>
+                <img class="imgArticle" src="img/captain_america.jpg" alt="capitan america en lucha">
+              </div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  <br><br>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+              <button type="button" name="button">leer mas</button>
+            </article>
+            <article class="personajes">
+              <div class="box">
+                <h3>personaje 03</h3>
+                <img class="imgArticle" src="img/captain_america.jpg" alt="capitan america en lucha">
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                <br><br>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+              <button type="button" name="button">leer mas</button>
+            </article>
+            <article class="personajes">
+              <div class="box">
+                <h3>personaje 04</h3>
+                <img class="imgArticle" src="img/captain_america.jpg" alt="capitan america en lucha">
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                <br><br>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+              <button type="button" name="button">leer mas</button>
+            </article>
+            <article class="personajes">
+              <div class="box">
+                <h3>personaje 05</h3>
+                <img class="imgArticle" src="img/captain_america.jpg" alt="capitan america en lucha">
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                <br><br>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+              <button type="button" name="button">leer mas</button>
+            </article>
+        </section>
+        <aside class="peliculas dos">
+          <section>
+          	<header><h2>peliculas</h2></header>
+          	<article>
+          		<header>
+          			<h1>Noticia 1</h1>
+          			<p>Texto descriptivo 1</p>
+          		</header>
+          		<footer>
+              		    <p>Publicado el XXX del XXX de XXX</p>
+            		</footer>
+          	</article>
 
-
-      </section>
-
-    </main>
+          	<article>
+          		<header>
+          			<h1>Noticia 2</h1>
+          			<p>Texto descriptivo 2</p>
+          		</header>
+          		<footer>
+              		    <p>Publicado el XXX del XXX de XXX</p>
+            		</footer>
+          	</article>
+          </section>
+        </aside>
+        <footer class="background tres">
+        </footer>
+      </main>
 
 
 
